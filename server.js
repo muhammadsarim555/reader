d5ab4bac6101e1a9a50c0adf4e3d00b696e6549f
muhammadsarim555 <muhammadsarim555@gmail.com>
2020-01-23 20:17:10 -0800

    First node app deploy

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const http = require("http");
-const app = require("./app");
+// const http = require("http");
+// const app = require("./app");
 
-const port = process.env.port || 3000;
-const server = http.createServer(app);
+// const port = process.env.port || 3000;
+// const server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log("Server connected to localhost!");
+// server.listen(port, () => {
+//   console.log("Server connected to localhost!");
+// });
+const express = require('express');
+
+const app = express();
+const path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Your node js server is running');
 });
