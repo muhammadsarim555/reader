382a0bee776d95dafb79ddfd292878358e70b80a
muhammadsarim555 <muhammadsarim555@gmail.com>
2020-01-23 20:20:06 -0800

    Change server flow

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-// const http = require("http");
-// const app = require("./app");
+const http = require("http");
+const app = require("./app");
 
-// const port = process.env.port || 3000;
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
 // server.listen(port, () => {
 //   console.log("Server connected to localhost!");
 // });
-const express = require('express');
 
-const app = express();
-const path = require('path');
+const path = require("path");
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log('Your node js server is running');
+server.listen(process.env.PORT || 3000, function() {
+  console.log("Your node js server is running");
 });
